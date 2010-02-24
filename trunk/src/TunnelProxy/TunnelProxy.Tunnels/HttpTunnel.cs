00f05d10a43c5a924250083ed63e19402339d240
jacobladams <jacobladams@24fe9a6c-1fda-11df-8c3c-e1b4bc802e70>
2010-02-24 14:04:58 +0000

    Added server project and ideas for shared projects.

---
+++ trunk/src/TunnelProxy/TunnelProxy.Tunnels/HttpTunnel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TunnelProxy.Interfaces;
+
+namespace TunnelProxy.Tunnels
+{
+	public class HttpTunnel:ITunnel
+	{
+		public Uri Address { get; set; }
+
+		public HttpTunnel(Uri address)
+		{
+			Address = address;
+		}
+
+		#region ITunnel Members
+
+		public void Send(byte[] data)
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte[] Receive()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
