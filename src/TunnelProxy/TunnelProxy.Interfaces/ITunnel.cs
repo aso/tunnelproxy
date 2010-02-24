0a1d8a6c87f914234a7f10fc9cf558c751782fbf
jacobladams <jacobladams@24fe9a6c-1fda-11df-8c3c-e1b4bc802e70>
2010-02-24 14:04:58 +0000

    Added server project and ideas for shared projects.

---
+++ src/TunnelProxy/TunnelProxy.Interfaces/ITunnel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TunnelProxy.Interfaces
+{
+	public interface ITunnel
+	{
+		void Send(byte[] data);
+		byte[] Receive();
+	}
+}
