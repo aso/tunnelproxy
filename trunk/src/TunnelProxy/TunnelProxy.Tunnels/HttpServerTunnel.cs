93434630b983402dbd7b6e19f28ee6f90bea91af
jacobladams <jacobladams@24fe9a6c-1fda-11df-8c3c-e1b4bc802e70>
2010-03-06 23:09:12 +0000

    Implemented crude http communication between client and server

---
+++ trunk/src/TunnelProxy/TunnelProxy.Tunnels/HttpServerTunnel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TunnelProxy.Interfaces;
+using System.Net;
+using System.IO;
+
+namespace TunnelProxy.Tunnels
+{
+	public class HttpServerTunnel : ITunnel
+	{
+		private HttpListener _httpListener = new HttpListener();
+		private HttpListenerContext _tempContext;
+
+		public HttpServerTunnel(string prefix)
+		{
+			_httpListener.Prefixes.Add(prefix);
+
+
+			_httpListener.Start();
+
+			_httpListener.BeginGetContext(new AsyncCallback(GetContextCallBack), null);
+
+		}
+
+		private void GetContextCallBack(IAsyncResult result)
+		{
+			Stream inputStream = null;
+			StreamReader reader = null;
+			try
+			{
+				_tempContext = _httpListener.EndGetContext(result);
+				inputStream = _tempContext.Request.InputStream;
+				reader = new StreamReader(inputStream);
+				string request = reader.ReadToEnd();
+				byte[] data = System.Text.Encoding.UTF8.GetBytes(request);
+				//inputStream.Read(data, 0, data.Length);
+				if (DataReceived != null)
+					DataReceived(this, new DataReceivedEventArgs(data));
+
+			}
+			finally
+			{
+				if (inputStream != null)
+					inputStream.Close();
+				if(reader!=null)
+					reader.Close();
+			}
+		}
+
+		#region ITunnel Members
+
+		public void Send(byte[] data)
+		{
+			HttpListenerResponse response = null;
+			Stream dataStream = null;
+			try
+			{
+				response = _tempContext.Response;
+				response.ContentLength64 = data.Length;
+				dataStream = response.OutputStream;
+				dataStream.Write(data, 0, data.Length);
+
+				_httpListener.BeginGetContext(new AsyncCallback(GetContextCallBack), null);
+			}
+			finally
+			{
+				if (dataStream != null)
+					dataStream.Close();
+				if (response != null)
+					response.Close();
+			}
+
+		}
+		public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+
+		#endregion
+
+
+	}
+}
--- trunk/src/TunnelProxy/TunnelProxy.Tunnels/HttpTunnel.cs
+++ trunk/src/TunnelProxy/TunnelProxy.Tunnels/HttpTunnel.cs
@@ -3,30 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TunnelProxy.Interfaces;
+using System.Net;
+using System.IO;
 
 namespace TunnelProxy.Tunnels
 {
-	public class HttpTunnel:ITunnel
+	public class HttpTunnel : ITunnel
 	{
 		public Uri Address { get; set; }
+		public string RequestMethod { get; set; }
 
-		public HttpTunnel(Uri address)
+		public HttpTunnel(Uri address, string requestMethod)
 		{
 			Address = address;
+			RequestMethod = requestMethod;
 		}
 
 		#region ITunnel Members
 
 		public void Send(byte[] data)
 		{
-			throw new NotImplementedException();
-		}
+			WebResponse response = null;
+			Stream dataStream = null;
+			StreamReader reader = null;
+			try
+			{
+				WebRequest request = WebRequest.Create(Address);
+				request.Method = RequestMethod;
+				//request.ContentType = "application/x-www-form-urlencoded";
+				request.ContentLength = data.Length;
+				dataStream = request.GetRequestStream();
+				dataStream.Write(data, 0, data.Length);
+				dataStream.Close();
 
-		public byte[] Receive()
-		{
-			throw new NotImplementedException();
+				response = request.GetResponse();
+				dataStream = response.GetResponseStream();
+
+				reader = new StreamReader(dataStream);
+				string responseString = reader.ReadToEnd();
+				byte[] results = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+				//byte[] results = new byte[dataStream.Length - 1];
+
+				//dataStream.Read(results, 0, results.Length);
+				if (DataReceived != null)
+					DataReceived(this, new DataReceivedEventArgs(results));
+
+			}
+			finally
+			{
+				if (dataStream != null)
+					dataStream.Close();
+				if (response != null)
+					response.Close();
+			}
+
 		}
+		public event EventHandler<DataReceivedEventArgs> DataReceived;
+
 
 		#endregion
+
+
 	}
 }
