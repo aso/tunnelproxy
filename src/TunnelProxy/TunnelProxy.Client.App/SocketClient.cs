7e76838d985c7f0e59df54b9b1c0d547f749ce94
chris.chelmecki <chris.chelmecki@24fe9a6c-1fda-11df-8c3c-e1b4bc802e70>
2010-03-20 02:43:15 +0000

    It's a bit of a mess, but I wanted to check it in because it's working well with google setup for only 1 connection.  I'm going to try and get in cleaned up a little and add some kind of simple header so we can multiplex connections across the simple req/resp http connection.

--- src/TunnelProxy/TunnelProxy.Client.App/SocketClient.cs
+++ src/TunnelProxy/TunnelProxy.Client.App/SocketClient.cs
@@ -12,20 +12,12 @@ namespace TunnelProxy.Client.App
 
     class SocketClient
     {
-        public SocketClient(ITunnel Tunnel)
+        public SocketClient(ITunnel Tunnel, TcpClient tcpclient)
         {
-
+            this.client = tcpclient;
             this.Tunnel = Tunnel;
             //Register DataReceived handler with the tunnel
             Tunnel.DataReceived += new EventHandler<DataReceivedEventArgs>(Tunnel_DataReceived);
-
-            //open server socket
-
-            Int32 port = 13000;
-            IPAddress localAddr = IPAddress.Parse("192.168.1.102");
-
-            tcpListener = new TcpListener(localAddr, port);
-            tcpListener.Start();
         }
 
         public void HandleMessages()
@@ -34,21 +26,23 @@ namespace TunnelProxy.Client.App
             Byte[] temp = new Byte[10240];
             Byte[] bytes;
 
-            //Wait for new connection
-            client = tcpListener.AcceptTcpClient();
+
             networkStream = client.GetStream();
 
             // Loop to receive all the data sent by the client.
-            while (client.Connected)
+            while (client.Connected && !disconnect)
             {    
                 i = networkStream.Read(temp, 0, temp.Length);
-                bytes = new byte[i];
-                Array.Copy(temp, 0, bytes, 0, i);
 
-                System.Console.WriteLine("--->Sent {0} bytes to server", bytes.Length);
-                System.Console.WriteLine(ConversionUtils.ConvertToString(bytes));
-                Tunnel.Send(bytes);
+                if (i > 0)
+                {
+                    bytes = new byte[i];
+                    Array.Copy(temp, 0, bytes, 0, i);
 
+                    System.Console.WriteLine("--->Sent {0} bytes to server", bytes.Length);
+                    System.Console.WriteLine(ConversionUtils.ConvertToString(bytes));
+                    Tunnel.Send(bytes);
+                }
             }
 
             // Shutdown and end connection
@@ -59,14 +53,14 @@ namespace TunnelProxy.Client.App
 		{
 			byte[] data = e.Data;
             System.Console.WriteLine("<---Recvd {0} bytes from server", data.Length);
-            networkStream.Write(data, 2, data.Length - 2);
+            networkStream.Write(data, 2, data.Length - 2);           
 		}
 
         //Member Objects
         ITunnel Tunnel;
-        TcpListener tcpListener;
         TcpClient client;
         NetworkStream networkStream;
+        bool disconnect = false;
     }
 
 
--- src/TunnelProxy/TunnelProxy.Tunnels/HttpTunnel.cs
+++ src/TunnelProxy/TunnelProxy.Tunnels/HttpTunnel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using TunnelProxy.Interfaces;
 using System.Net;
 using System.IO;
+using System.Threading;
 using TunnelProxy.Util;
 
 namespace TunnelProxy.Tunnels
@@ -18,6 +19,11 @@ namespace TunnelProxy.Tunnels
 		{
 			Address = address;
 			RequestMethod = requestMethod;
+
+            Thread bgThread = new Thread(new ThreadStart(PollingLoop));
+            bgThread.Name = "PollingThread";
+            bgThread.Start();
+
 		}
 
 		#region ITunnel Members
@@ -26,14 +32,22 @@ namespace TunnelProxy.Tunnels
 		{
 			WebResponse response = null;
 			Stream dataStream = null;
+            byte[] temp = new byte[data.Length + 2];
+
+            Array.Copy(data, 0, temp, 2, data.Length);
+
+            while (waiting) Thread.Sleep(1);
+
+            waiting = true;
+
 			try
 			{
 				WebRequest request = WebRequest.Create(Address);
 				request.Method = RequestMethod;
 				//request.ContentType = "application/x-www-form-urlencoded";
-				request.ContentLength = data.Length;
+				request.ContentLength = temp.Length;
 				dataStream = request.GetRequestStream();
-				dataStream.Write(data, 0, data.Length);
+				dataStream.Write(temp, 0, temp.Length);
 				dataStream.Close();
 
 				response = request.GetResponse();
@@ -53,12 +67,26 @@ namespace TunnelProxy.Tunnels
 					response.Close();
 			}
 
+            waiting = false;
+
 		}
+
+        public void PollingLoop()
+        {
+            Byte[] temp = new Byte[2];
+            while (true)
+            {
+                Thread.Sleep(500);                
+                Send(temp);
+             }
+
+        }
+
 		public event EventHandler<DataReceivedEventArgs> DataReceived;
 
 
 		#endregion
 
-
+        bool waiting = false;
 	}
 }
